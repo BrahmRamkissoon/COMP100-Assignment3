441b2cdf3375a574a016fb99e5a9c3728a6205d6
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-12 09:23:42 -0400

    Initial Commit

---
+++ Assignment3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main ( string [] args )
+        {
+            int count = 0;  // sentinel variable
+
+            while ( count != 30 ) // condition to check
+            {
+                Console.WriteLine($"Playing count: {count}");
+                count++;    //increment by 1
+            }
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Please press any key to exit ...");
+            Console.ReadKey();
+        }
+    }
+}
